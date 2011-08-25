24e340a9d60d739dce8bceeed4aa02ba02ca095c
Moonok Kim <misok79@hotmail.com>
2011-08-25 11:42:55 -0700

    adding more files

---
+++ RadSL_current/WorldMapControlRadSL/CountryCoordinate.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Telerik.Windows.Controls.Map;
+using System.Windows.Data;
+using System.Linq;
+using System.Xml.Linq;
+using System.ServiceModel;
+using System.Collections.Generic;
+using System.Windows;
+using System.IO;
+
+namespace WorldMapControlRadSL
+{
+    public class Country
+    {
+        private string _CountryName;
+        public string CountryName
+        {
+            get { return this._CountryName; }
+            set { this._CountryName = value; }
+        }
+
+        private double _Longitude;
+        public double Longitude
+        {
+            get { return this._Longitude; }
+            set { this._Longitude = value; }
+        }
+
+        private double _Latitude;
+        public double Latitude
+        {
+            get { return this._Latitude; }
+            set { this._Latitude = value; }
+        }
+    }
+
+    public class CountryCoordinate
+    {
+        static public List<Country> countryList;
+
+        static public void LoadCountryCoordinate()
+        {
+            Stream fileInfo = typeof(CountryCoordinate).Assembly.GetManifestResourceStream("WorldMapControlRadSL.country_coordinate.csv");
+
+            if (fileInfo == null)
+            {
+                return;
+            }
+
+            if (countryList == null)
+            {
+                countryList = new List<Country>();
+            }
+            using (StreamReader fileReader = new StreamReader(fileInfo))
+            {
+                string line = string.Empty;
+                string[] row;
+                while ((line = fileReader.ReadLine()) != null)
+                {
+                    row = line.Split(',');
+                    Country nCountry = new Country();
+                    nCountry.CountryName = (row[0] == null) ? string.Empty : row[0];
+                    nCountry.Latitude =  Convert.ToDouble((row[1] == null) ? string.Empty : row[1]);
+                    nCountry.Longitude = Convert.ToDouble((row[2] == null) ? string.Empty : row[2]);
+
+                    countryList.Add(nCountry);
+                }
+            }
+        }
+    }
+}
---
+++ RadSL_current/WorldMapControlRadSL/SharepointListItem.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace WorldMapControlRadSL
+{
+    public class SharepointListItem
+    {
+        // CountryName
+        private string _Title;
+        public string Title
+        {
+            get { return this._Title; }
+            set { this._Title = value; }
+        }
+
+        private string _CountryNameAbbreviation;
+        public string CountryNameAbbreviation
+        {
+            get { return this._CountryNameAbbreviation; }
+            set { this._CountryNameAbbreviation = value; }
+        }
+
+        private string _DataField1;
+        public string DataField1
+        {
+            get { return this._DataField1; }
+            set { this._DataField1 = value; }
+        }
+
+        private string _DataField2;
+        public string DataField2
+        {
+            get { return this._DataField2; }
+            set { this._DataField2 = value; }
+        }
+
+        private string _DataField3;
+        public string DataField3
+        {
+            get { return this._DataField3; }
+            set { this._DataField3 = value; }
+        }
+
+        private string _DataField4;
+        public string DataField4
+        {
+            get { return this._DataField4; }
+            set { this._DataField4 = value; }
+        }
+
+        private string _DataField5;
+        public string DataField5
+        {
+            get { return this._DataField5; }
+            set { this._DataField5 = value; }
+        }
+
+        private string _DashboardLink;
+        public string DashboardLink
+        {
+            get { return this._DashboardLink; }
+            set { this._DashboardLink = value; }
+        }
+
+        private string _Clickable;
+        public string Clickable
+        {
+            get { return this._Clickable; }
+            set { this._Clickable = value; }
+        }
+
+        public SharepointListItem()
+        {
+        }
+
+        public SharepointListItem(string title, string countrynameabbrev, string datafield1, string datafield2, string datafield3, string datafield4, string datafield5, string dashboardlink, string clickable)
+        {
+            this._Title = title;
+            this._CountryNameAbbreviation = countrynameabbrev;
+            this._DataField1 = datafield1;
+            this._DataField2 = datafield2;
+            this._DataField3 = datafield3;
+            this._DataField4 = datafield4;
+            this._DataField5 = datafield5;
+            this._DashboardLink = dashboardlink;
+            this._Clickable = clickable;
+        }
+    }
+}
